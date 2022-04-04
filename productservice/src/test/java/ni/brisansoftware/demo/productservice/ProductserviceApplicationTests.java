1eba5d5ff3838695d222ba33cafc0264d0b2449c
Francisco Briceno <fjavier.cgam@gmail.com>
2022-04-04 15:13:49 -0600

    First Commit for the productservice

---
+++ productservice/src/test/java/ni/brisansoftware/demo/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ni.brisansoftware.demo.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
